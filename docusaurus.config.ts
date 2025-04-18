4ad240ac71ca70a25f34d7a5856ceec19ffd69eb
lxd520 <106861392+lxd-520@users.noreply.github.com>
2025-04-18 10:32:29 +0800

    Update docusaurus.config.ts
    网站基础参数配置

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,51 +8,89 @@ const beian = '闽ICP备2020017848号-3'
 const beian1 = '闽公网安备35021102000847号'
 
 const config: Config = {
+//恒量配置
   title: '鸿安泰达',
+//标题
   url: 'https://www.hatd.eu.org/',
+//链接
   baseUrl: '/',
+//基础网址
   favicon: 'img/favicon.ico',
+//图标：img/图标文件
   organizationName: 'HATD',
-  projectName: 'blog',
+//组织名称:HATD
+  projectName: 'home',
+//项目名称：首页
   customFields: {
+//自定义字段
     bio: '道阻且长，行则将至',
+//生物
     description:
+//描述
       '致力于公共安全领域，生产及销售公、检、法、司、武警、反恐、部队及相关部门用于安全检查、排爆、反恐、缉毒、治安、刑侦、通讯、救援、户外等业务所需装备器材。',
   },
+
+
   themeConfig: {
+   //主题配置
     // announcementBar: {
+   //公告栏
     //   id: 'announcementBar-3',
+   //公告3
     //   content: ``,
+   //主要内容
     // },
     image: 'img/og.png',
+//图片
     metadata: [
+//元数据
       {
         name: 'author',
+//姓名
         content: '鸿安泰达',
+//内容
       },
       {
         name: 'keywords',
+//关键字
         content: 'blog, javascript, typescript, node, react, vue, web',
+//内容：博客、 javascript, typescript, node, react, vue, web
       },
+
       {
         name: 'keywords',
+//关键字
         content: '编程爱好者, Web开发者, 写过爬虫, 学过逆向, 主攻ts全栈',
+//内容：编程爱好者, Web开发者, 写过爬虫, 学过逆向, 主攻ts全栈
       },
     ],
+
     docs: {
+//文档
       sidebar: {
+//侧边栏
         hideable: true,
+//可隐藏：真
       },
     },
+
     navbar: {
+//导航栏
       logo: {
-        alt: '愧怍',
+//标志
+        alt: '鸿安泰达',
+//
         src: 'img/logo.webp',
+//浅色模式：图标logo
         srcDark: 'img/logo.webp',
+//暗色模式：图标logo
       },
       hideOnScroll: true,
+//滚动时隐藏
       items: [
+//项目
         { label: '首页', position: 'right', to: 'home' },
+//标签：首页，位置：右  
         {
           label: '产品中心',
           position: 'right',
@@ -68,32 +106,51 @@ const config: Config = {
 
         // {
         //   type: 'localeDropdown',
+//类型：下拉菜单
         //   position: 'right',
+//位置：右
         // },
       ],
     },
+
+
+
+
+
+
     footer: {
+//页脚
       style: 'dark',
+//风格：深色
       links: [
+//链接
         {
           title: '学习',
+//标题：学习
           items: [
-            { label: '博客', to: 'blog' },
-            { label: '归档', to: 'blog/archive' },
-            { label: '实战项目', to: 'project' },
-            { label: '前端示例', to: 'https://example.kuizuo.cn' },
+//项目
+            { label: '首页', to: 'home' },
+//标签：首页，致，主页
+            { label: '警用装备', to: 'product/police' },
+//
+            { label: '战术装备', to: 'product/tactics' },
+            { label: '户外装备', to: 'product/outdoor' },
           ],
         },
+
+
         {
           title: '社交媒体',
           items: [
-            { label: '关于我', to: '/about' },
+            { label: '关于我们', to: '/about' },
             { label: 'GitHub', href: social.github.href },
             { label: 'Twitter', href: social.x.href },
             { label: '掘金', href: social.juejin.href },
             { label: 'Discord', href: social.discord.href },
           ],
         },
+
+
         {
           title: '网站',
           items: [
